9a1762382bb6dc5ad456cf7f11f2b2e5a11304a8
Wesley Junio <wesjrpiri@gmail.com>
2026-04-14 19:16:46 +0100

    feat: create controller to get products from external api.

--- src/main/java/dev/java/ecommerce/basketservice/client/PlatziStoreClient.java
+++ src/main/java/dev/java/ecommerce/basketservice/client/PlatziStoreClient.java
@@ -1,6 +1,6 @@
-package client;
+package dev.java.ecommerce.basketservice.client;
 
-import client.response.PlatziProductResponse;
+import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +12,6 @@ public interface PlatziStoreClient {
     @GetMapping("/products")
     List<PlatziProductResponse> getAllProducts();
 
-    @GetMapping("/{id}")
+    @GetMapping("/products/{id}")
     PlatziProductResponse getProductById(@PathVariable Long id);
 }
